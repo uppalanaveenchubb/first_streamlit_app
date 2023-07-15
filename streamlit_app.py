1fd507e770cc75bbd5bdd7a9b875ec0d9fa775c6
uppalanaveenchubb <139229475+uppalanaveenchubb@users.noreply.github.com>
2023-07-15 17:38:44 +0530

    Update streamlit_app.py
    Added images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 Import stremlit
 streamlit.title('My Parents New Healthy dite')
+streamlit.header('Breakfast Favorites')
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Omega 3 Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket Smooethie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 Blueberry Oatmeal')
+streamlit.text('🥗 Kale,Spinach and Rocket Smooethie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocadao Toast')
