da3c1e4c33f40620ad73ae694850d2d244d90fab
uppalanaveenchubb <139229475+uppalanaveenchubb@users.noreply.github.com>
2023-08-01 23:08:18 +0530

    Update streamlit_app.py
    added snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My Parents New Healthy dite')
 streamlit.header('Breakfast Favorites')
 streamlit.header('Breakfast Menu')
