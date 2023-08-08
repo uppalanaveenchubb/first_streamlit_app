72d6b67063ce736a279ad0253275858488dd31fb
uppalanaveenchubb <139229475+uppalanaveenchubb@users.noreply.github.com>
2023-08-08 17:42:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,6 @@ try:
         streamlit.error("Please select a fruit to get information. ")
    else:    
         back_from_function= get_fruityvice_data(fruit_choice) 
-    
-
-
 streamlit.header("The fruit load list contains:")
 #snowflake related function
 def get_fruit_load_list():
