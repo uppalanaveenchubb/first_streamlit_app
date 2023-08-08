27116e56d39566c51aff13f288ec473bbf18d0d5
uppalanaveenchubb <139229475+uppalanaveenchubb@users.noreply.github.com>
2023-08-08 11:06:59 +0530

    Update streamlit_app.py
    try catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,25 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # Let's put a pick list here so they can pick the fruit they want to include 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
 #New Section to display fruityvice api response
-
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try: 
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+    streamlit.error("Please select a fruit to get information. ")
+ else    
+    #streamlit.write('The user entered ', fruit_choice)
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Displays in readable format
+     streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLerror as e:
+ streamlit.error()
+
 
 streamlit.stop()
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Displays in readable format
-streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
